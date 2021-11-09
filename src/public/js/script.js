1a0094601874defd835c1cb48b51a2ae80059aca
kahseng <S10208180@connect.np.edu.sg>
2021-11-09 10:50:19 +0800

    Add drag to scroll to volunteer events and community section

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,5 +1,39 @@
+let isDown = false, startX, scrollLeft;
+
+function handleDown(e) {
+    isDown = true;
+    $(this).addClass('active');
+    startX = e.pageX - $(this).offset().left;
+    scrollLeft = $(this).scrollLeft();
+}
+
+function handleLeave() {
+    isDown = false;
+    $(this).removeClass('active');
+}
+
+function handleUp() {
+    isDown = false;
+    $(this).removeClass('active');
+}
+
+function handleMove(e) {
+    if(!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - $(this).offset().left;
+    const walk = (x - startX) * 2;
+    $(this).scrollLeft(scrollLeft - walk);
+}
+
 $(document).ready(function() {
     $("button.mobile-menu-button").on("click", function() {
         $(".mobile-menu").toggle("hidden")
     })
+    
+    $('.slide').on({
+        "mousedown": handleDown,
+        'mouseleave': handleLeave,
+        'mouseup': handleUp,
+        'mousemove': handleMove
+    });
 })
