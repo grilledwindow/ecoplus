282d6db201c0ec3143f4c2d06ac7e8f75f4fd65d
Xavier Ang <xaveang@gmail.com>
2021-11-12 16:03:07 +0800

    Fix typo

--- src/sql/triggers.sql
+++ src/sql/triggers.sql
@@ -1,4 +1,4 @@
-DROP TRIGGER IF EXISTS trig_create_user_from_auth ON auth.users;
+DROP TRIGGER IF EXISTS create_user_from_auth ON auth.users;
 DROP FUNCTION IF EXISTS public.create_user_from_auth;
 
 CREATE FUNCTION public.create_user_from_auth() RETURNS trigger AS $$
@@ -8,7 +8,3 @@ CREATE FUNCTION public.create_user_from_auth() RETURNS trigger AS $$
     RETURN NEW;
   END;
 $$ LANGUAGE plpgsql SECURITY DEFINER;
-
-CREATE TRIGGER trig_create_user_from_auth
-AFTER INSERT ON auth.users
-  FOR EACH ROW EXECUTE FUNCTION public.trig_create_user_from_auth();
