a764dc1e610933a5cfe349bd2c1dbf442f424806
Xavier Ang <xaveang@gmail.com>
2021-11-12 16:03:29 +0800

    Add update trigger on public.users

--- src/sql/triggers.sql
+++ src/sql/triggers.sql
@@ -1,6 +1,9 @@
 DROP TRIGGER IF EXISTS create_user_from_auth ON auth.users;
 DROP FUNCTION IF EXISTS public.create_user_from_auth;
 
+DROP TRIGGER IF EXISTS on_user_update ON public.users;
+DROP FUNCTION IF EXISTS public.on_user_update;
+
 CREATE FUNCTION public.create_user_from_auth() RETURNS trigger AS $$
   BEGIN
     INSERT INTO public.users (id)
@@ -8,3 +11,18 @@ CREATE FUNCTION public.create_user_from_auth() RETURNS trigger AS $$
     RETURN NEW;
   END;
 $$ LANGUAGE plpgsql SECURITY DEFINER;
+
+CREATE TRIGGER create_user_from_auth
+AFTER INSERT ON auth.users
+  FOR EACH ROW EXECUTE FUNCTION public.create_user_from_auth();
+
+CREATE FUNCTION public.on_user_update() RETURNS trigger AS $$
+  BEGIN
+    NEW.updated_at := timezone('gmt'::text, now());
+    RETURN NEW;
+  END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER on_user_update
+BEFORE UPDATE ON public.users
+  FOR EACH ROW EXECUTE FUNCTION public.on_user_update();
