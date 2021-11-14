f655ec727eaefc1100e4d3a0a0b7c1d0e11a2337
kahseng <S10208180@connect.np.edu.sg>
2021-11-15 01:18:03 +0800

    Connect to supabase database

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -36,4 +36,51 @@ $(document).ready(function() {
         "mouseup": handleUp,
         "mousemove": handleMove
     });
+
+    // TODO: Case: if there are no events
+
+    fetch("/api/view-events")
+    .then((res) => res.json())
+    .then(({data}) => {
+        data.map((event) => {
+            $("#event-mini-browse").append(`
+            <div class="event-card">
+                <div class="h-48"></div>
+                <div class="bg-white p-4 w-96 min-w-full rounded-b-md">
+                    <h2 class="text-xl font-bold">${event.name}</h2>
+                    <p>${event.location}</p>
+                    <p>${new Date(event.datetime).toLocaleDateString("en-US", { weekday: "long", year: "numeric", month:"long", day:"numeric"})}</p>
+                    <a class="btn-primary flex items-center justify-center mt-8">
+                        More Info
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
+                        </svg>
+                    </a>
+                </div>
+            </div>
+            `)
+        })
+    })
+
+    // TODO: Case: if there are no communities
+
+    fetch("/api/view-communities")
+    .then((res) => res.json())
+    .then(({data}) => {
+        data.map((community) => {
+            $("#community-mini-browse").append(`
+            <div class="community-card">
+                <div class="w-88 min-w-full">
+                    <h2 class="text-xl font-bold">${community.name}</h2>
+                    <a class="btn-primary flex items-center justify-center mt-8">
+                        More Info
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
+                        </svg>
+                    </a>
+                </div>
+            </div>
+            `)
+        })
+    })
 })
