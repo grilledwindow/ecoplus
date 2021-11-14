3c28bdd8c3051f9796b30c81a984c096405882c4
kahseng <S10208180@connect.np.edu.sg>
2021-11-15 02:15:46 +0800

    Connect community page to database

--- src/public/js/community.js
+++ src/public/js/community.js
@@ -46,25 +46,27 @@ $(document).ready(function() {
             $(".modal").addClass("hidden")
         });
     });
-    
+
     // TODO: Case: if there are no communities
 
     fetch("/api/view-communities")
     .then((res) => res.json())
     .then(({data}) => {
+        console.log(data)
         data.map((community) => {
-            $("#community-mini-browse").append(`
-            <div class="community-card">
-                <div class="w-88 min-w-full">
-                    <h2 class="text-xl font-bold">${community.name}</h2>
-                    <a class="btn-primary flex items-center justify-center mt-8">
-                        More Info
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
-                        </svg>
-                    </a>
+            $("#community-browse").append(`
+                <div class="text-lg bg-gray-200 border-4 border-gray-200 rounded-lg">
+                    <div class="h-48"></div>
+                    <div class="bg-white p-4 w-96 min-w-full rounded-b-md">
+                        <h2 class="font-bold">${community.name}</h2>
+                        <a class="btn-primary flex items-center justify-center mt-8">
+                            More Info
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
+                            </svg>
+                        </a>
+                    </div>
                 </div>
-            </div>
             `)
         })
     })
