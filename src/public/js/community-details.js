e5ea634d06226182b5ee8041f79ae26191910875
kahseng <S10208180@connect.np.edu.sg>
2021-11-16 20:23:10 +0800

    Fix event create page

--- src/public/js/community-details.js
+++ src/public/js/community-details.js
@@ -81,7 +81,7 @@ $(document).ready(function() {
                         <h2 class="text-xl font-bold">${event.name}</h2>
                         <p>${event.location}</p>
                         <p>${new Date(event.datetime).toLocaleDateString('en-SG', { day: '2-digit', month: 'long', year: 'numeric', hour: '2-digit'})}</p>
-                        <a class="btn-primary flex items-center justify-center mt-8">
+                        <a class="btn-primary flex items-center justify-center mt-8" href="./event-details.html?id=${event.id}">
                             More Info
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
