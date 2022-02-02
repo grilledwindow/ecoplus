9e48f6a513ef862091eed50f92cf7758ed3ca8d8
Xavier Ang <xaveang@gmail.com>
2022-02-02 12:36:37 +0800

    Update views

--- src/sql/views.sql
+++ src/sql/views.sql
@@ -1,7 +1,7 @@
 DROP VIEW IF EXISTS events_posts_view;
 CREATE VIEW events_posts_view
 AS
-  SELECT ep.event_id, u.name, u.username, u.has_img, ep.post
+  SELECT ep.event_id, ep.user_id, u.name, u.username, u.has_img, ep.post, ep.created_at
   FROM users u
   INNER JOIN events_posts ep
   ON ep.user_id = u.id;
@@ -9,7 +9,7 @@ AS
 DROP VIEW IF EXISTS communities_posts_view;
 CREATE VIEW communities_posts_view
 AS
-  SELECT cp.community_id, u.name, u.username, u.has_img, cp.post
+  SELECT cp.community_id, cp.user_id, u.name, u.username, u.has_img, cp.post, cp.created_at
   FROM users u
   INNER JOIN communities_posts cp
   ON cp.user_id = u.id;
