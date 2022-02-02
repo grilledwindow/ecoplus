ef5ea1cc2983437f64f665758939dbc759c0483d
kahseng <S10208180@connect.np.edu.sg>
2022-02-03 01:34:49 +0800

    Add display community images

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -28,27 +28,48 @@ $(document).ready(function() {
         }
     })
 
+    fetchCommunities()
+})
+
+function fetchCommunities() {
+    let communityRow = $("#community-mini-browse")
+    communityRow.html("")
+
     // fetch list of communities
     fetch("/api/view-communities")
     .then((res) => res.json())
     .then(({data}) => {
+        var htmlString = ""
+
         if (data.length != 0) {
-            $("#community-mini-browse").html("")
             data.map((community) => {
-                $("#community-mini-browse").append(`
-                <div class="community-card">
-                    <div class="w-88 min-w-full">
-                        <h2 class="text-xl font-bold">${community.name}</h2>
-                        <a class="btn-primary flex items-center justify-center mt-8" href="./community-details.html?id=${community.id}">
-                            More Info
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
-                            </svg>
-                        </a>
-                    </div>
-                </div>
-                `)
+                htmlString += `
+                    <div class="event-card bg-white">
+                        <div class="h-48 bg-gray-200">`
+                    
+                if (community.has_cover_img) {
+                    htmlString += `<img class="object-cover rounded-lg h-full w-full" src="https://stolploftqaslfirbfsf.supabase.in/storage/v1/object/public/public/communities/cover_img/${community.id}.jpg"/>`
+                }
+
+                if (community.has_profile_img) {
+                    htmlString += `<img class="object-cover mx-auto -mt-40 rounded-full h-32 w-32 bg-gray-400" src="https://stolploftqaslfirbfsf.supabase.in/storage/v1/object/public/public/communities/profile_img/${community.id}.jpg"/>`
+                }
+
+                htmlString += `
+                        </div>
+                        <div class="p-4 w-96 min-w-full rounded-b-md">
+                            <h2 class="text-xl font-bold">${community.name}</h2>
+                            <a class="btn-primary flex items-center justify-center mt-8" href="./community-details.html?id=${community.id}">
+                                More Info
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
+                                </svg>
+                            </a>
+                        </div>
+                    </div>`
             })
         }
+
+        communityRow.html(htmlString)
     })
-})
+}
