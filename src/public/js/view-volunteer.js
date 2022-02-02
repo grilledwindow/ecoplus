4930d1ea216f5ac186019e8cfcaf8eef612af536
kahseng <S10208180@connect.np.edu.sg>
2022-02-03 04:48:09 +0800

    Add event image upload function

--- src/public/js/view-volunteer.js
+++ src/public/js/view-volunteer.js
@@ -2,9 +2,145 @@ let parameters = new URLSearchParams(window.location.search)
 const event_id = parameters.get("id")
 const user_id = sessionStorage.getItem("userID")
 
+$(document).ready(function() {
+    
+    if (sessionStorage.getItem("userID") == null) {
+        window.location.href = "../index.html";
+    }
+
+    fetch("/api/event-details", {
+        method: "POST",
+        body: JSON.stringify({
+            event_id
+        })
+    })
+    .then((res) => res.json())
+    .then(({data}) => {
+        if (data.event[0].owner_id != user_id) {
+            window.location.href = "../index.html"
+        }
+
+        data.event.map((event) => {
+            $("#event-name").html(event.name)
+            $("#event-description").html(event.description)
+            $("#event-datetime").html(new Date(event.datetime).toLocaleDateString('en-SG', { day: '2-digit', month: 'long', year: 'numeric', hour: '2-digit'}))
+            
+            let daysRemaining = Math.ceil((new Date(event.datetime) - new Date()) / (1000 * 60 * 60 * 24))
+            
+            if (daysRemaining < 0)
+                $("#days-remaining").html("0")
+            else
+                $("#days-remaining").html(daysRemaining)
+
+
+            if (event.has_img) {
+                let url = `https://stolploftqaslfirbfsf.supabase.in/storage/v1/object/public/public/events/${event.id}.jpg`
+                $(".event-img").attr('src', url)
+            }
+
+            $("#edit-event-button").html(`
+                <a class="mt-8 btn-primary flex items-center max-w-min cursor-pointer" href="../event-edit.html?id=${event.id}">
+                    <svg class="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>
+                    <span>Edit Event</span>
+                </a>
+            `)
+
+            $("#delete-event").show()
+
+            $("#delete-event-button").on("click", () => {
+                $("#modal-bg").show()
+                $("#delete-event-modal-form").show()
+            })
+
+            $("#change-event-img-button").on("click", () => {
+                $("#modal-bg").show()
+                $("#change-event-img-modal-form").show()
+            })
+        
+            $("#modal-bg").click(hideModal)
+            $(".modal-cancel").click(hideModal)
+        
+            $("#modal-delete").on("click", () => {
+                $("#error-message").html("")
+                if (event.name == $("#delete-event-input").val()) {
+                    fetch("/api/event-delete", {
+                        method: "POST",
+                        body: JSON.stringify({
+                            event_id
+                        })
+                    })
+                    .then((res) => res.json())
+                    .then((data) => {
+                        window.location.href = "../account.html";
+                    })
+                }
+                
+                else $("#error-message").html("You didn't enter the event name correctly")
+            })
+
+            $("#modal-change-img").on("click", () => {
+                $("#event-img-error-message").html("")
+                const imgInput = document.getElementById("change-img-input");
+                const img = imgInput.files[0];
+                const session = JSON.parse(localStorage.getItem("session"));
+
+                if (img) {
+                    const reader = new FileReader();
+
+                    reader.onload = function (re) {
+                        const binaryString = re.target.result;
+                        // encode as base64 because Netlify doesn't support img uploads properly
+                        const base64string = btoa(binaryString);
+
+                        fetch('/api/event-img', {
+                            method: 'POST',
+                            body: JSON.stringify({
+                                session,
+                                event_id,
+                                img: base64string
+                            })
+                        })
+                        .then(res => res.json())
+                        .then(data => {
+
+                            // Update session
+                            localStorage.setItem("session", JSON.stringify(data.session));
+
+                            // Refresh so changes can be seen automatically
+                            location.reload();
+                        })
+                        .catch(console.error);
+                    };
+
+                    reader.readAsBinaryString(img);
+                }
+                else $("#event-img-error-message").html("Please locate a image to be set")
+            })
+        })
+    })
+
+    fetch("/api/event-users", {
+        method: "POST",
+        body: JSON.stringify({
+            event_id
+        })
+    })
+    .then((res) => res.json())
+    .then(({data}) => {
+        data.map(({users}) => {
+            $("#event-users").append(`
+                <tr>
+                    <td class="px-6 py-3 whitespace-nowrap text-gray-500">${users.name}</td>
+                </tr>
+            `)        
+        })
+    })
+})
+
 function hideModal() {
     $("#modal-bg").hide()
     $("#delete-event-modal-form").hide()
+    $("#change-event-img-modal-form").hide()
 }
 
 function searchFunction() {
@@ -29,82 +165,3 @@ function searchFunction() {
         }
     }
 }
-
-$(document).ready(function() {
-    fetch("/api/event-details", {
-        method: "POST",
-        body: JSON.stringify({
-            event_id
-        })
-    })
-    .then((res) => res.json())
-    .then(({data}) => {
-        data.event.map((event) => {
-            $("#event-name").html(event.name)
-            $("#event-description").html(event.description)
-            $("#event-datetime").html(new Date(event.datetime).toLocaleDateString('en-SG', { day: '2-digit', month: 'long', year: 'numeric', hour: '2-digit'}))
-            
-            let daysRemaining = Math.ceil((new Date(event.datetime) - new Date()) / (1000 * 60 * 60 * 24))
-            
-            if (daysRemaining < 0)
-                $("#days-remaining").html("0")
-            else
-                $("#days-remaining").html(daysRemaining)
-
-            if (event.owner_id == user_id) {
-                $("#edit-event-button").html(`
-                    <a class="mt-8 btn-primary flex items-center max-w-min cursor-pointer" href="../event-edit.html?id=${event.id}">
-                        <svg class="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>
-                        <span>Edit Event</span>
-                    </a>
-                `)
-
-                $("#delete-event").show()
-            }
-
-            $("#delete-event-button").on("click", () => {
-                $("#modal-bg").show()
-                $("#delete-event-modal-form").show()
-            })
-        
-            $("#modal-bg").click(hideModal)
-            $(".modal-cancel").click(hideModal)
-        
-            $("#modal-delete").on("click", () => {
-                $("#error-message").html("")
-                if (event.name == $("#delete-event-input").val()) {
-                    fetch("/api/event-delete", {
-                        method: "POST",
-                        body: JSON.stringify({
-                            event_id
-                        })
-                    })
-                    .then((res) => res.json())
-                    .then((data) => {
-                        window.location.href = "../account.html";
-                    })
-                }
-                
-                else $("#error-message").html("You didn't enter the event name correctly")
-            })
-        
-        })
-    })
-
-    fetch("/api/event-users", {
-        method: "POST",
-        body: JSON.stringify({
-            event_id
-        })
-    })
-    .then((res) => res.json())
-    .then(({data}) => {
-        data.map(({users}) => {
-            $("#event-users").append(`
-                <tr>
-                    <td class="px-6 py-3 whitespace-nowrap text-gray-500">${users.name}</td>
-                </tr>
-            `)        
-        })
-    })
-})
